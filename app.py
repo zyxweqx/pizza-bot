27cd0805dd80f013dd98243861da4ee814de9033
zyxweqx <nazyabarya228@gmail.com>
2026-04-16 12:20:14 +0200

    feat: added admin router

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from aiogram.types import BotCommandScopeAllPrivateChats
 
 from dotenv import load_dotenv,find_dotenv
 
+from handlers.admin_private import admin_router
 from handlers.user_group import user_group_router
 from handlers.user_private import user_private_router
 from common.bot_cmds_list import private
@@ -25,6 +26,7 @@ dp = Dispatcher()
 
 dp.include_router(user_private_router)
 dp.include_router(user_group_router)
+dp.include_router(admin_router)
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
