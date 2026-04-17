ce2055d898147d2103e212db8e6465dcf41f1efc
zyxweqx <nazyabarya228@gmail.com>
2026-04-17 14:41:41 +0200

    feat: implement full CRUD operations for products

--- database/orm_query.py
+++ database/orm_query.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.models import Product
@@ -11,4 +12,29 @@ async def orm_add_product(session: AsyncSession, data: dict):
         image=data['image']
     )
     session.add(obj)
+    await session.commit()
+
+async def orm_get_products(session: AsyncSession):
+    query = select(Product)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+async def orm_get_product(session: AsyncSession, product_id: int):
+    query = select(Product).where(Product.id == product_id)
+    result = await session.execute(query)
+    return result.scalar_one_or_none()
+
+async def orm_update_product(session: AsyncSession, product_id: int, data):
+    query = update(Product).where(Product.id == product_id).values(
+        name=data['name'],
+        description=data['description'],
+        price=float(data['price']),
+        image=data['image'],
+    )
+    result = await session.execute(query)
+    await session.commit()
+
+async def orm_delete_product(session: AsyncSession, product_id: int):
+    query = delete(Product).where(Product.id == product_id)
+    await session.execute(query)
     await session.commit()
