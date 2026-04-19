7b7d604ca7126d2723592695d0a88ac8bee38e7e
zyxweqx <nazyabarya228@gmail.com>
2026-04-19 12:02:52 +0200

    feat: migrate to PostgreSQL and add requirements.txt

--- database/engine.py
+++ database/engine.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 
 from database.models import Base
 
-engine = create_async_engine(os.getenv('DB_LITE'), echo=True)
+engine = create_async_engine(os.getenv('DB_URL'), echo=True)
 
 session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
