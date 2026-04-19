5de2f0faab14cc17209cbdb38e863191180d4ef5
zyxweqx <nazyabarya228@gmail.com>
2026-04-19 12:59:43 +0200

    refactor: simplify user private router and replace menu with counter example

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,91 +1,33 @@
-from aiogram import types, Router, F
-from aiogram.filters import CommandStart, Command, or_f
-from aiogram.utils.formatting import as_marked_section, Bold, as_list
+from aiogram import F, types, Router
+from aiogram.filters import CommandStart
+
 from sqlalchemy.ext.asyncio import AsyncSession
+from database.orm_query import (
+    orm_add_to_cart,
+    orm_add_user,
+)
 
-from database.orm_query import orm_get_products
 from filters.chat_types import ChatTypeFilter
-
-from kbds import reply
-from kbds.reply import get_keyboard
+from kbds.inline import get_callback_btns
 
 user_private_router = Router()
-user_private_router.message.filter(ChatTypeFilter(['private']))
+user_private_router.message.filter(ChatTypeFilter(["private"]))
+
 
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer("Hi, I'm virtual helper!",
-                         reply_markup=get_keyboard(
-                             "Menu",
-                             "About shop",
-                             "Payment options",
-                             "Shipping options",
-                             "Send phone number",
-                             placeholder="What are you interested in?",
-                             sizes=(2, 2)
-                         ),
-                )
+    await message.answer("Hello! I'm your virtual assistant",
+                         reply_markup=get_callback_btns(btns={
+                             'Click me': 'some_1'
+                         }))
 
 
-@user_private_router.message(or_f(Command('menu'), (F.text.lower() == "menu")))
-async def menu_cmd(message: types.Message, session: AsyncSession):
-    await message.answer("Here is menu: ℹ️")
-    for product in await orm_get_products(session):
-        await message.answer_photo(
-            product.image,
-            caption=f"<strong>{product.name}\
-               </strong>\n{product.description}\nPrice: {round(product.price, 2)}",
-        )
+@user_private_router.callback_query(F.data.startswith('some_'))
+async def counter(callback: types.CallbackQuery):
+    number = int(callback.data.split('_')[-1])
 
-
-
-@user_private_router.message((F.text.lower() == "about shop"))
-@user_private_router.message(Command("about"))
-async def about_cmd(message: types.Message):
-    await message.answer("About us: ")
-
-@user_private_router.message(or_f(Command('payment'), (F.text.lower().contains('payment'))))
-@user_private_router.message(Command('payment'))
-async def payment_cmd(message: types.Message):
-
-    text = as_marked_section(
-            Bold("Payment options:"),
-            "By card in bot",
-            "Cash",
-            "In shop",
-            marker="✅"
-        )
-    await message.answer(text.as_html())
-
-@user_private_router.message((F.text.lower().contains('shippi')) | (F.text.lower() == 'shipping options'))
-@user_private_router.message(Command('shipping'))
-async def shipping_cmd(message: types.Message):
-
-    text = as_list(
-        as_marked_section(
-            Bold("Shipping options/order:"),
-            "Courier",
-            "I'll run over and pick it up now",
-            "I'll eat at your place ",
-            marker="✅ "
-        ),
-        as_marked_section(
-            Bold("Forbidden:"),
-            "Email",
-            "Pigeons",
-            marker="❌ "
-        ),
-        sep='\n---------------------\n'
-
-    )
-    await message.answer(text.as_html())
-
-@user_private_router.message(F.contact)
-async def get_contact_cmd(message: types.Message):
-    await message.answer("Here is contact info: ")
-    await message.answer(str(message.contact))
-
-@user_private_router.message(F.location)
-async def get_location(message: types.Message):
-    await message.answer("Here is location info: ")
-    await message.answer(str(message.location))
+    await callback.message.edit_text(
+        text=f"Total clicks: {number}",
+        reply_markup=get_callback_btns(btns={
+            'Click again': f'some_{number + 1}'
+        }))
