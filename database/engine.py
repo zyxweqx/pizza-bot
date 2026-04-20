653f7df62e5112f1446551f5e32783dcbc6a0bda
zyxweqx <nazyabarya228@gmail.com>
2026-04-20 13:13:53 +0200

    chore: add initial data seeding to create_db

--- database/engine.py
+++ database/engine.py
@@ -1,19 +1,24 @@
 import os
-
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from database.models import Base
+from database.orm_query import orm_add_banner_description, orm_create_categories
 
-engine = create_async_engine(os.getenv('DB_URL'), echo=True)
+from common.texts_for_db import categories, description_for_info_pages
+
+engine = create_async_engine(os.getenv('DB_LITE'), echo=True)
 
 session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
+
 async def create_db():
-    # noinspection PyTypeChecker
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
+    async with session_maker() as session:
+        await orm_create_categories(session, categories)
+        await orm_add_banner_description(session, description_for_info_pages)
+
 async def drop_db():
-    # noinspection PyTypeChecker
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
