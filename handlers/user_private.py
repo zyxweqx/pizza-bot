f8fd8b4be65f0f1eb69061ddab78a24d681ca4ee
zyxweqx <nazyabarya228@gmail.com>
2026-05-01 14:08:43 +0200

    feat: add to cart functionality

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -2,6 +2,7 @@ from aiogram import F, types, Router
 from aiogram.filters import CommandStart
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from database.orm_query import orm_add_user, orm_add_to_cart
 from filters.chat_types import ChatTypeFilter
 from handlers.menu_processing import get_menu_content
 from kbds.inline import MenuCallBack
@@ -16,14 +17,32 @@ async def start_cmd(message: types.Message, session: AsyncSession):
 
     await message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
 
+async def add_to_cart(callback: types.CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
+    user = callback.from_user
+    await orm_add_user(
+        session,
+        user_id=user.id,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        phone=None,
+    )
+    await orm_add_to_cart(session, user_id=user.id, product_id=callback_data.product_id)
+    await callback.answer("Product added to cart!")
+
+
 @user_private_router.callback_query(MenuCallBack.filter())
 async def user_menu(callback: types.CallbackQuery,callback_data: MenuCallBack, session: AsyncSession):
+
+    if callback_data.menu_name == "add_to_cart":
+        await add_to_cart(callback, callback_data, session)
+        return
 
     media, reply_markup = await get_menu_content(
         session,
         level=callback_data.level,
         menu_name=callback_data.menu_name,
         category=callback_data.category,
+        page=callback_data.page,
     )
 
     await callback.message.edit_media(media=media, reply_markup=reply_markup)
